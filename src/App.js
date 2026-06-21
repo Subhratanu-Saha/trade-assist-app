f018da18f86a6a7814d8e5ae0deb13fdca0cba46
Subhamoy Saha <subhamoysaha778@gmail.com>
2026-06-21 09:43:30 +0530

    Fix punctuation in welcome message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Welcome to Trade Assist! Powered by an Engineering team
+          Welcome to Trade Assist! Powered by an Engineering team.
         </p>
       </header>
     </div>
