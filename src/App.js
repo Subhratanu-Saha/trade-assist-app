8fec1ae9f2cc3059d821a95cbe4e64556f02568d
Subhajit Kumar Das <dassubhajit695@gmail.com>
2026-07-12 19:46:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   
 
   return (
-    <div className="min-h-screen bg-slate-100 flex items-center justify-center px-4">
-      <header className="w-full max-w-xl rounded-2xl bg-slate-800 px-8 py-10 text-center text-white shadow-xl">
+    <div >
+      <header >
         <PasswordInput
           
         />
