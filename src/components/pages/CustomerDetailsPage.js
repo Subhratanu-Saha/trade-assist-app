aaed0f061c8a1a48a90aed113bf0673619058734
Subhamoy Saha <subhamoysaha778@gmail.com>
2026-09-24 17:15:42 +0530

    feat: add button to create new case in Customer Details page

--- src/components/pages/CustomerDetailsPage.js
+++ src/components/pages/CustomerDetailsPage.js
@@ -68,7 +68,13 @@ const CustomerDetailsPage = () => {
 
             {/* Case */}
             {activeTab === "case" && (
-              <div className="border border-dashed border-gray-300 p-6 text-center">
+              <div className="relative border border-dashed border-gray-300 p-6 pt-16 text-center">
+                <button
+                  type="button"
+                  className="absolute right-4 top-4 rounded-md bg-[#0A2947] px-3 py-2 text-sm font-semibold text-white"
+                >
+                  + New Case
+                </button>
                 <p className="text-sm text-gray-500">
                   No case details available.
                 </p>
